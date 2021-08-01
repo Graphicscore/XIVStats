0407d7c384244ba7ce845a8ec7eb271e32f45714
Dominik Louven <louven.dominik@gmail.com>
2021-08-01 18:04:32 +0200

    Migrate population chart to echart.js

--- js/xivdata.js
+++ js/xivdata.js
@@ -132,25 +132,24 @@ $(function() {
     $("#label_realm_japan_active").text(data.realms.japan.active.count.toLocaleString('en'));
     $("#label_realm_europe_active").text(data.realms.europe.active.count.toLocaleString('en'));
 
-    createPopulationChart($("#canvas_population_america_all")[0].getContext('2d'),data.realms.america.all.distribution)
-    createPopulationChart($("#canvas_population_america_active")[0].getContext('2d'),data.realms.america.active.distribution)
+    createPopulationChart($("#div_population_america_all")[0],data.realms.america.all.distribution)
+    createPopulationChart($("#div_population_america_active")[0],data.realms.america.active.distribution)
 
-    createPopulationChart($("#canvas_population_japan_all")[0].getContext('2d'),data.realms. japan.all.distribution)
-    createPopulationChart($("#canvas_population_japan_active")[0].getContext('2d'),data.realms.japan.active.distribution)
+    createPopulationChart($("#div_population_japan_all")[0],data.realms. japan.all.distribution)
+    createPopulationChart($("#div_population_japan_active")[0],data.realms.japan.active.distribution)
 
-    createPopulationChart($("#canvas_population_europe_all")[0].getContext('2d'),data.realms.europe.all.distribution)
-    createPopulationChart($("#canvas_population_europe_active")[0].getContext('2d'),data.realms.europe.active.distribution)
+    createPopulationChart($("#div_population_europe_all")[0],data.realms.europe.all.distribution)
+    createPopulationChart($("#div_population_europe_active")[0],data.realms.europe.active.distribution)
 
-    createGenderRaceChart($("#canvas_population_race_all")[0].getContext('2d'),data.racedistribution.all)
+    /*createGenderRaceChart($("#canvas_population_race_all")[0].getContext('2d'),data.racedistribution.all)
     createGenderRaceChart($("#canvas_population_race_active")[0].getContext('2d'),data.racedistribution.active)
 
     createJobChart($("#canvas_population_job_all")[0].getContext('2d'),data.jobs.all)
     createJobChart($("#canvas_population_job_active")[0].getContext('2d'),data.jobs.active)
 
     createGrandCompanyChart($("#canvas_population_gc_all")[0].getContext('2d'),data.grandcompany.all)
-    createGrandCompanyChart($("#canvas_population_gc_active")[0].getContext('2d'),data.grandcompany.active)
+    createGrandCompanyChart($("#canvas_population_gc_active")[0].getContext('2d'),data.grandcompany.active)*/
 });
-
 
 function createBarChartOptions() {
     return {
@@ -301,7 +300,7 @@ function createGenderRaceChart(ctx,data){
 }
 
 
-function createPopulationChart(ctx, populationData)
+function createPopulationChart(div, populationData)
 {
 
     var labels = [];
@@ -313,21 +312,65 @@ function createPopulationChart(ctx, populationData)
         values.push(populationData[i].count);
     }
 
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: '# of Characters',
-                data: values,
-                backgroundColor: [
-                    'rgba(112, 38, 112)'
-                ],
-                borderWidth: 0
-            }]
-        },
-        options: createBarChartOptions()
-    });
+    var myChart = echarts.init(div);
+    var option;
+
+option = {
+    color: ["rgba(112, 38, 112)"],
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'shadow'
+        }
+    },
+    textStyle: {
+        color: 'rgba(255, 255, 255, 0.7)',
+        fontSize: 32,
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            data: labels,
+            axisTick: {
+                alignWithLabel: true
+            },
+            axisLabel: {
+                fontSize: 12,
+                rotate: 30,
+                interval: 0,
+                margin: 12
+            }
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value',
+            axisLabel: {
+                fontSize: 15,
+                formatter: function(value, index){
+                    return humanFormat(value, {
+                        separator: ''
+                    })
+                }
+            }
+        }
+    ],
+    series: [
+        {
+            name: '# of Characters',
+            type: 'bar',
+            barWidth: '60%',
+            data: values,
+        }
+    ]
+};
+option && myChart.setOption(option);
 }
 
 function createJobChart(ctx, jobData){
