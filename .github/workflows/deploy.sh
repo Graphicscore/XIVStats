76298790022596742f065967df0da08f94228289
Dominik Louven <louven.dominik@gmail.com>
2021-08-17 17:42:33 +0200

    Add --ignore-stdin to http in deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -7,12 +7,12 @@ echo "Checking dependencies...."
 sudo apt-get -y install httpie jq
 
 echo "Requesting Webservice Token..."
-PORTAINER_TOKEN=$(http POST "$PORTAINER_AUTH_URL" Username="$PORTAINER_USER" Password="$PORTAINER_PASSWORD" | jq -r '.jwt')
+PORTAINER_TOKEN=$(http --ignore-stdin POST "$PORTAINER_AUTH_URL" Username="$PORTAINER_USER" Password="$PORTAINER_PASSWORD" | jq -r '.jwt')
 PORTAINER_TOKEN_HEADER="Authorization: Bearer $PORTAINER_TOKEN"
 
 echo "Pull latest image..."
 #pull image
-http POST "$PORTAINER_DOCKER_URL/images/create?fromImage=ghcr.io%2Fgraphicscore%2Fxivstats:latest" "$PORTAINER_TOKEN_HEADER"
+http --ignore-stdin POST "$PORTAINER_DOCKER_URL/images/create?fromImage=ghcr.io%2Fgraphicscore%2Fxivstats:latest" "$PORTAINER_TOKEN_HEADER"
 
 echo "Request information about currently running container..."
 #get info about currently running ffxivstats container and save json response
@@ -20,11 +20,11 @@ CONTAINER_JSON=$(http GET "$PORTAINER_DOCKER_URL/containers/$CONTAINER_NAME/json
 
 echo "Stopping currently running container..."
 #stop currently running container
-http POST "$PORTAINER_DOCKER_URL/containers/$CONTAINER_NAME/stop" "$PORTAINER_TOKEN_HEADER"
+http --ignore-stdin POST "$PORTAINER_DOCKER_URL/containers/$CONTAINER_NAME/stop" "$PORTAINER_TOKEN_HEADER"
 
 echo "Renaming old container ..."
 #rename old container
-http POST "$PORTAINER_DOCKER_URL/containers/$CONTAINER_NAME/rename?name=$CONTAINER_NAME-old" "$PORTAINER_TOKEN_HEADER"
+http --ignore-stdin POST "$PORTAINER_DOCKER_URL/containers/$CONTAINER_NAME/rename?name=$CONTAINER_NAME-old" "$PORTAINER_TOKEN_HEADER"
 
 echo "Performing jq magic to generate json payload for container recreation..."
 #Get Config objects content and use that as a base for the target json
@@ -41,9 +41,9 @@ echo "Re-Creating container ..."
 echo $TARGET_JSON | http POST "$PORTAINER_DOCKER_URL/containers/create?name=$CONTAINER_NAME" "$PORTAINER_TOKEN_HEADER"
 
 echo "Starting container ..."
-http POST "$PORTAINER_DOCKER_URL/containers/$CONTAINER_NAME/start" "$PORTAINER_TOKEN_HEADER"
+http --ignore-stdin POST "$PORTAINER_DOCKER_URL/containers/$CONTAINER_NAME/start" "$PORTAINER_TOKEN_HEADER"
 
 echo "Deleting old container ..."
-http DELETE "$PORTAINER_DOCKER_URL/containers/$CONTAINER_NAME-old?force=true&v=1" "$PORTAINER_TOKEN_HEADER"
+http --ignore-stdin DELETE "$PORTAINER_DOCKER_URL/containers/$CONTAINER_NAME-old?force=true&v=1" "$PORTAINER_TOKEN_HEADER"
 
 echo "OK Deploy done!"
